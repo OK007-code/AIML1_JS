fc07cec25a5666bfd712d2cf1e1e2a872b45439e
Omika Kumar <kumaromika007@gmail.com>
2026-02-20 10:32:50 +0530

    javanewfaile

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+// console.log("Hello World");
+/*var num = 5;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "Hello World";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if (sym1 === sym2) {
+  console.log("True");
+} else {
+  console.log("False");
+}*/
+var a = 100; var b = 200; var c = 300; var linebreak = "<br>";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
